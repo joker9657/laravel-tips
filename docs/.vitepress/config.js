e3c7f0c539cf713a3022ec5f4031ece2ae0503f1
joker9657 <zqf2102398@gmail.com>
2023-07-06 20:05:40 +0800

    fix: vue语法{{ }} 冲突

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -4,9 +4,6 @@ export default defineConfig({
     title: 'laravel-tips',
     description: 'Awesome Laravel tips and tricks for all artisans.',
     themeConfig: {
-        nav: [
-            { text: 'Example', link: '/README' },
-        ],
         sidebar: [
             {
                 // text: 'Guide',
@@ -15,6 +12,18 @@ export default defineConfig({
                 items: [
                     { text: '数据库模型', link: '/db-models-and-eloquent'},
                     { text: '模型关联', link: '/models-relations'},
+                    { text: '数据库迁移', link: '/migrations'},
+                    { text: '视图', link: '/views'},
+                    { text: '路由', link: '/routing'},
+                    { text: '表单验证', link: '/validation'},
+                    { text: '集合', link: '/collections'},
+                    { text: '用户授权', link: '/auth'},
+                    { text: '发送邮件', link: '/mail'},
+                    { text: 'Artisan 命令行', link: '/artisan'},
+                    { text: '工厂模式', link: '/factories'},
+                    { text: '日志和调试', link: '/log-and-debug'},
+                    { text: 'API', link: '/api'},
+                    { text: '其他', link: '/other'},
                 ],
             }
         ],
